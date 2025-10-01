3d6c616ce687ffb010518336418ad7fb40d13cfa
amirabayoumi <149677044+amirabayoumi@users.noreply.github.com>
2025-10-01 17:37:22 +0200

    fix Navbar in small screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,58 +10,51 @@ export const Navbar = () => {
   };
 
   return (
-    <header className="flex justify-between items-center p-4 bg-dark-purple  md:text-xl">
-      <div className=" flex items-center text-yellow">
-        <Activity className="text-yellow w-10 h-10" />
+    <header className="flex flex-col md:flex-row justify-between items-center p-4 bg-dark-purple md:text-xl">
+      {/* Logo / Title */}
+      <div className="flex items-center text-yellow w-full justify-between md:w-auto">
+        <div className="flex items-center">
+          <Activity className="text-yellow w-10 h-10" />
+          <p className="ml-2">Together, we decide</p>
+        </div>
 
-        <p className="">Together, we decide</p>
+        <button className="text-yellow md:hidden" onClick={toggleMenu}>
+          {isOpen ? <span>&#10005;</span> : <MenuIcon className="w-6 h-6" />}
+        </button>
       </div>
-      <ul className=" list-none  text-yellow hidden md:flex">
-        <li className="mr-4">
-          <Link href="/"> Home</Link>
-        </li>
 
+      <ul className="list-none text-yellow hidden md:flex">
         <li className="mr-4">
-          <Link href="/ideas"> Report & Improve </Link>
+          <Link href="/">Home</Link>
         </li>
         <li className="mr-4">
-          <Link href="/poll"> Vote Now </Link>
+          <Link href="/ideas">Report & Improve</Link>
         </li>
         <li className="mr-4">
-          <Link href="/contact"> Contact</Link>
+          <Link href="/poll">Vote Now</Link>
+        </li>
+        <li className="mr-4">
+          <Link href="/contact">Contact</Link>
         </li>
       </ul>
-      {/* mobile menu */}
-      <div className="md:hidden">
-        <button className="text-yellow" onClick={toggleMenu}>
-          {isOpen ? (
-            <span className="">&#10005;</span>
-          ) : (
-            <MenuIcon className="w-6 h-6" />
-          )}
-        </button>
-        {isOpen && (
-          <div className="absolute right-0 mt-2 w-48 bg-dark-purple text-yellow rounded-md shadow-lg">
-            <ul className="list-none">
-              <li className="p-2 hover:bg-dark-purple/50">
-                <Link href="/"> Home</Link>
-              </li>
-              <li className="p-2 hover:bg-dark-purple/50">
-                <Link href="/about"> About </Link>
-              </li>
-              <li className="p-2 hover:bg-dark-purple/50">
-                <Link href="/ideas">Report & Improve </Link>
-              </li>
-              <li className="p-2 hover:bg-dark-purple/50">
-                <Link href="/poll"> Vote Now </Link>
-              </li>
-              <li className="p-2 hover:bg-dark-purple/50">
-                <Link href="/contact"> Contact</Link>
-              </li>
-            </ul>
-          </div>
-        )}
-      </div>
+
+      {isOpen && (
+        <ul className="flex flex-col w-full mt-4 text-yellow md:hidden  text-center ">
+          <li className="p-2 hover:bg-dark-purple/50 border-t border-gray-600">
+            <Link href="/" onClick={toggleMenu}>Home</Link>
+          </li>
+
+          <li className="p-2 hover:bg-dark-purple/50 border-t border-gray-600">
+            <Link href="/ideas" onClick={toggleMenu}>Report & Improve</Link>
+          </li>
+          <li className="p-2 hover:bg-dark-purple/50 border-t border-gray-600">
+            <Link href="/poll" onClick={toggleMenu}>Vote Now</Link>
+          </li>
+          <li className="p-2 hover:bg-dark-purple/50 border-t border-gray-600">
+            <Link href="/contact" onClick={toggleMenu}>Contact</Link>
+          </li>
+        </ul>
+      )}
     </header>
   );
 };
